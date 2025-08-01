cf2fa1de0badf30b79fd0ffe6a1f03d6ff73cb0b
checoluis2212 <luissergio.mkt@gmail.com>
2025-07-31 20:52:15 -0600

    Update Response.js

--- models/Response.js
+++ models/Response.js
@@ -13,7 +13,7 @@ const responseSchema = new mongoose.Schema({
   buttonCounts: {
     cotizar:       { type: Number, default: 0 },
     publicar:      { type: Number, default: 0 },
-    oportunidades: { type: Number, default: 0 },
+    empleo: { type: Number, default: 0 },
   },
   metadata: {
     ip:        String,
