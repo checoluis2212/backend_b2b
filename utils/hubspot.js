257e2f83c46b1ee39616972da519127a247d7462
checoluis2212 <luissergio.mkt@gmail.com>
2025-08-07 17:33:04 -0600

    Update hubspot.js

--- utils/hubspot.js
+++ utils/hubspot.js
@@ -2,16 +2,15 @@ import axios from 'axios';
 
 const HUBSPOT_TOKEN = process.env.HUBSPOT_TOKEN;
 if (!HUBSPOT_TOKEN) {
-  throw new Error('❌ Falta la variable de entorno HUBSPOT_TOKEN');
+  throw new Error('❌ Falta la variable HUBSPOT_TOKEN');
 }
-
 const BASE_URL = 'https://api.hubapi.com';
 
 /**
  * Upsert de contacto en HubSpot por email, añadiendo visitor_id
  */
 export async function upsertHubspotContact(email, visitorId, extra = {}) {
-  const url = `${BASE_URL}/crm/v3/objects/contacts?hapikey=${HUBSPOT_TOKEN}&idProperty=email`;
+  const url = `${BASE_URL}/crm/v3/objects/contacts?idProperty=email`;
   const payload = {
     properties: {
       email,
@@ -20,6 +19,9 @@ export async function upsertHubspotContact(email, visitorId, extra = {}) {
     }
   };
   return axios.post(url, payload, {
-    headers: { 'Content-Type': 'application/json' }
+    headers: {
+      'Content-Type':  'application/json',
+      'Authorization': `Bearer ${HUBSPOT_TOKEN}`
+    }
   });
 }
