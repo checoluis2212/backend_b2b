97d42fc6d9e1360864c321a28a2360913821f84a
checoluis2212 <luissergio.mkt@gmail.com>
2025-08-07 17:32:21 -0600

    Update Response.js

--- models/Response.js
+++ models/Response.js
@@ -8,40 +8,30 @@ const utmSchema = new mongoose.Schema({
   content:  String,
 }, { _id: false });
 
-// Sub-schema para cada envío de formulario
 const contactSchema = new mongoose.Schema({
   name:      String,
   email:     String,
-  payload:   mongoose.Schema.Types.Mixed, // cualquier otro campo
+  payload:   mongoose.Schema.Types.Mixed,
   createdAt: { type: Date, default: Date.now }
 }, { _id: false });
 
 const responseSchema = new mongoose.Schema({
   visitorId: { type: String, required: true, index: true },
 
-  // Contadores de botones existentes
   buttonCounts: {
     cotizar:  { type: Number, default: 0 },
     publicar: { type: Number, default: 0 },
     empleo:   { type: Number, default: 0 },
   },
 
-  // Metadatos originales
   metadata: {
     ip:        String,
     referer:   String,
     utmParams: utmSchema,
   },
 
-  // Nuevo: envíos de formularios
-  contacts: {
-    type: [contactSchema],
-    default: []
-  },
-  submissionCount: {
-    type: Number,
-    default: 0
-  }
+  contacts:       { type: [contactSchema], default: [] },
+  submissionCount:{ type: Number, default: 0 }
 
 }, { timestamps: true });
 
