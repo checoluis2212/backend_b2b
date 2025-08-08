fa6b1b8e476e9d828e58ef87085dd4fb7484c60d
checoluis2212 <luissergio.mkt@gmail.com>
2025-08-08 14:34:29 -0600

    Update hubspot.js

--- services/hubspot.js
+++ services/hubspot.js
@@ -1,3 +1,4 @@
+// services/hubspot.js
 import axios from 'axios';
 
 const HS_TOKEN = process.env.HUBSPOT_TOKEN;
@@ -25,4 +26,42 @@ export async function findContactByEmail(email) {
   });
   const results = res.data.results;
   return results && results.length ? results[0] : null;
+}
+
+/**
+ * Crea un contacto en HubSpot con las propiedades dadas
+ */
+export async function createContact(properties) {
+  const url = 'https://api.hubapi.com/crm/v3/objects/contacts';
+  const res = await axios.post(url, { properties }, {
+    headers: {
+      'Content-Type':  'application/json',
+      'Authorization': `Bearer ${HS_TOKEN}`
+    }
+  });
+  return res.data;
+}
+
+/**
+ * Busca o crea un contacto en HubSpot, devuelve { id, properties }
+ */
+export async function upsertContactByEmail(fieldsObj) {
+  const email = fieldsObj.email;
+  // 1) Intenta buscar
+  const existing = await findContactByEmail(email);
+
+  if (existing) {
+    // Si existe, devolvemos su id y properties
+    return {
+      id: existing.id,
+      properties: existing.properties
+    };
+  } else {
+    // Si no existe, lo creamos
+    const created = await createContact(fieldsObj);
+    return {
+      id: created.id,
+      properties: created.properties
+    };
+  }
 }
